1fb24a89ef97ee86909affe6cf35f3b6e2e7dc3b
ValeryGiselleChumpitazCaycho <111779657+ValeryChumpitaz@users.noreply.github.com>
2025-03-14 11:07:10 -0500

    Update Gato.java

--- src/main/java/pe/edu/vallegrande/polimorfismo/Gato.java
+++ src/main/java/pe/edu/vallegrande/polimorfismo/Gato.java
@@ -2,8 +2,8 @@ package pe.edu.vallegrande.polimorfismo;
 
 // 🔹 Polimorfismo (Subclase)
 public class Gato extends Animal {
-    // Método que hace el sonido específico de un gato
-    @Override
+    // Método que hace el sonido específico de un gato , el Override permite sobreescribir para realizar su propia implementacion
+    @Override 
     public void hacerSonido() {
         System.out.println("Miau miau!");
     }
